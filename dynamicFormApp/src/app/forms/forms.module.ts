faa7172d41e8239a077d2adc5585075c80841e91
OrlaColeman <48183159+OrlaColeman@users.noreply.github.com>
2019-06-17 10:34:01 +0100

    creating components

---
+++ dynamicFormApp/src/app/forms/forms.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailFormComponent } from './email-form/email-form.component';
+
+@NgModule({
+  declarations: [EmailFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormsModule { }
